cc44b5494c210d4cdd654ad721f4e60dd44fa0d4
BlueExOo <ah440099@gmail.com>
2022-09-26 01:55:25 +0200

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedBackCard,
+  Footer,
+  Hero,
+  Stats,
+  Testimonials,
+};
